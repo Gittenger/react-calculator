749c596da26caeb04996eae3720290c968366665
Gittenger <john.pittenger86@gmail.com>
2019-06-13 15:03:29 -0400

    reformat, added stylesheet, ready for all react code

--- src/index.js
+++ src/index.js
@@ -2,76 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Buttons extends React.Component {
+class Container extends React.Component {
   constructor(props) {
     super(props);
-
-    this.getKeyType = getKeyType.bind(this);
-  }
-
-  getKeyType(key) {
-    console.log(key);
-    // let result = "number";
-    // switch (key.dataset.action) {
-    //   case "add":
-    //   case "subtract":
-    //   case "multiply":
-    //   case "divide":
-    //     result = "operator";
-    //     break;
-    //   case "decimal":
-    //     result = "decimal";
-    //     break;
-    //   case "clear":
-    //     result = "clear";
-    //     break;
-    //   case "calculate":
-    //     result = "calculate";
-    //     break;
-    //   default:
-    //     "number";
-    // }
-
-    // return result;
   }
 
   render() {
     return (
-      <div class="container">
-        <div
-          onClick={key => this.getKeyType(key)}
-          className="calculator__buttons"
-        >
-          <div className="calculator__keys">
-            <button className="key--operator" data-action="add">
-              +
-            </button>
-            <button className="key--operator" data-action="subtract">
-              -
-            </button>
-            <button className="key--operator" data-action="multiply">
-              x
-            </button>
-            <button className="key--operator" data-action="divide">
-              %
-            </button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>0</button>
-            <button data-action="decimal">.</button>
-            <button data-action="clear">AC</button>
-            <button className="key--equal" data-action="calculate">
-              =
-            </button>
-          </div>
-        </div>
+      <div className="container">
+        <Calculator />
+      </div>
+    );
+  }
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="calculator">
+        <Display />
+        <Buttons />
       </div>
     );
   }
@@ -85,16 +38,41 @@ class Display extends React.Component {
     };
   }
   render() {
-    return <div>{this.state.displayNum}</div>;
+    return <div className="calculator__display">{this.state.displayNum}</div>;
   }
 }
 
-class Calculator extends React.Component {
+class Buttons extends React.Component {
   render() {
     return (
-      <div>
-        <Display />
-        <Buttons />
+      <div className="calculator__keys">
+        <button className="key--operator" data-action="add">
+          +
+        </button>
+        <button className="key--operator" data-action="subtract">
+          -
+        </button>
+        <button className="key--operator" data-action="multiply">
+          x
+        </button>
+        <button className="key--operator" data-action="divide">
+          %
+        </button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>0</button>
+        <button data-action="decimal">.</button>
+        <button data-action="clear">AC</button>
+        <button className="key--equal" data-action="calculate">
+          =
+        </button>
       </div>
     );
   }
@@ -102,4 +80,4 @@ class Calculator extends React.Component {
 
 // ========================================
 
-ReactDOM.render(<Calculator />, document.getElementById("root"));
+ReactDOM.render(<Container />, document.getElementById("root"));
